d6eedab06f9e5dc92d47be0aaa18c971350cf339
Gabriel Lemus <glemus.stuart@gmail.com>
2022-11-20 17:59:40 -0600

    Add the file that contains the API structs for the requests and the responses

---
+++ Packet-Generator/server/src/api/packet_req_res.rs
@@ -0,0 +1,56 @@
+/// This file contains the strcuts used for the HTTP requests and responses.
+use serde::Serialize;
+
+/// Struct that represents the general TCP/IP packet sent by the API.
+#[derive(Serialize)]
+pub struct SentPacket {
+    /// Source IP address.
+    #[serde(rename = "sourceIP")]
+    pub source_ip: String,
+    /// Destination IP address.
+    #[serde(rename = "destinationIP")]
+    pub destination_ip: String,
+    /// IP version.
+    #[serde(rename = "IPVersion")]
+    pub ip_version: u8,
+    /// Port.
+    pub port: u16,
+    /// Data.
+    pub data: String,
+    /// Whether the packet was spoofed or not.
+    #[serde(rename = "isSpoofed")]
+    pub is_spoofed: bool,
+}
+
+/// Struct that represents the response sent by the API regarding the packet spoofing.
+#[derive(Serialize)]
+pub struct SpoofingResponse {
+    /// General informational message.
+    pub message: String,
+    /// Number of packets sent.
+    #[serde(rename = "packetCount")]
+    pub packet_count: u32,
+    /// List of packets sent.
+    #[serde(rename = "sentPackets")]
+    pub sent_packets: Vec<SentPacket>,
+}
+
+/// Struct that represents the general response sent by the API.
+#[derive(Serialize)]
+pub struct GeneralResponse {
+    /// General informational message.
+    pub message: String,
+    /// URL for the single request page.
+    #[serde(rename = "singleRequestPage")]
+    pub single_request_page: String,
+    /// URL for the multiple request page.
+    #[serde(rename = "multipleRequestPage")]
+    pub multiple_request_page: String,
+}
+
+/// Struct that represents the error response sent by the API.
+#[derive(Serialize)]
+pub struct ErrorResponse {
+    /// Error message.
+    pub error: String,
+}
