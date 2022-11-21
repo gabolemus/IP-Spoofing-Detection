6ad7a8ca015eab4de1eb0b13b6430f947894ef93
Gabriel Lemus <glemus.stuart@gmail.com>
2022-11-20 23:24:08 -0600

    Remove sent packet struct

--- Packet-Generator/server/src/api/packet_req_res.rs
+++ Packet-Generator/server/src/api/packet_req_res.rs
@@ -1,27 +1,6 @@
 /// This file contains the strcuts used for the HTTP requests and responses.
 use serde::Serialize;
 
-/// Struct that represents the general TCP/IP packet sent by the API.
-#[derive(Serialize)]
-pub struct SentPacket {
-    /// Source IP address.
-    #[serde(rename = "sourceIP")]
-    pub source_ip: String,
-    /// Destination IP address.
-    #[serde(rename = "destinationIP")]
-    pub destination_ip: String,
-    /// IP version.
-    #[serde(rename = "IPVersion")]
-    pub ip_version: u8,
-    /// Port.
-    pub port: u16,
-    /// Data.
-    pub data: String,
-    /// Whether the packet was spoofed or not.
-    #[serde(rename = "isSpoofed")]
-    pub is_spoofed: bool,
-}
-
 /// Struct that represents the response sent by the API regarding the packet spoofing.
 #[derive(Serialize)]
 pub struct SpoofingResponse {
@@ -30,9 +9,6 @@ pub struct SpoofingResponse {
     /// Number of packets sent.
     #[serde(rename = "packetCount")]
     pub packet_count: i32,
-    /// List of packets sent.
-    #[serde(rename = "sentPackets")]
-    pub sent_packets: Vec<SentPacket>,
 }
 
 /// Struct that represents the general response sent by the API.
