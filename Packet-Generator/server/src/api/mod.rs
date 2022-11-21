41bb063e4beefcf8fc65c0d2e36658a8123e517c
Gabriel Lemus <glemus.stuart@gmail.com>
2022-11-20 23:23:55 -0600

    Remove unused import

--- Packet-Generator/server/src/api/mod.rs
+++ Packet-Generator/server/src/api/mod.rs
@@ -1,9 +1,7 @@
 pub mod packet_req_res;
 pub mod routes;
 
-pub use packet_req_res::{
-    ErrorResponse, GeneralResponse, GenericResponse, SentPacket, SpoofingResponse,
-};
+pub use packet_req_res::{ErrorResponse, GeneralResponse, GenericResponse, SpoofingResponse};
 pub use routes::{
     index, multiple_requests, single_request, DESTINATION_IP_ADDRESS, PORT, SOURCE_IP_ADDRESS,
     STOP_INFINITE_PACKETS,
