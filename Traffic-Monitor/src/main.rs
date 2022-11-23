94be8027ad75674a84fe5578bf4ddee10405fccc
Gabriel Lemus <glemus.stuart@gmail.com>
2022-11-23 10:46:31 -0600

    Implement initial parsing of the pcap file as CSV

--- Traffic-Monitor/src/main.rs
+++ Traffic-Monitor/src/main.rs
@@ -1,48 +1,95 @@
-use pcap_parser::traits::PcapReaderIterator;
-use pcap_parser::*;
 use std::fs::File;
+use std::io::Write;
 
 fn main() {
-    let network_traffic = "../tcpdump/network-traffic.pcap";
-    let file = File::open(network_traffic).unwrap();
+    // Create a file to write the data to
+    let mut file = File::create("network-traffic.csv").unwrap();
 
-    let mut blocks_count = 0;
-    let mut reader = LegacyPcapReader::new(65536, file).expect("LegacyPcapReader");
+    // Creates a builder with needed tshark parameters
+    let builder =
+        rtshark::RTSharkBuilder::builder().input_path("./network-traffic.pcap");
 
-    loop {
-        match reader.next() {
-            Ok((offset, block)) => {
-                println!("");
-                blocks_count += 1;
+    // Start a new tshark process
+    let mut rtshark = builder
+        .spawn()
+        .unwrap_or_else(|e| panic!("Error starting tshark: {e}"));
 
-                match block {
-                    PcapBlockOwned::LegacyHeader(_header) => {
-                        // Print the header
-                        // println!("got header: {:?}", header);
+    let mut i = 1;
+    let mut is_first_packet = true;
 
-                        // Ignore the header for now
-                        blocks_count -= 1;
-                    }
+    // Read the packets until the end of the PCAP file
+    while let Some(packet) = rtshark.read().unwrap_or_else(|e| {
+        eprintln!("Error parsing tshark output: {e}");
+        None
+    }) {
+        // If this is not the first packet, check that the last line in the file
+        // is a new line. If it's not, write a new line
+        if !is_first_packet {
+            // Write a new line
+            file.write_all("\n".as_bytes()).unwrap();
+        }
 
-                    PcapBlockOwned::Legacy(block) => {
-                        // Print the block
-                        println!("Got block #{}: {:?}", blocks_count, block);
-                    }
+        if packet.layer_count() > 0 {
+            println!("Packet #{} analyzed", i);
+        }
 
-                    PcapBlockOwned::NG(_) => unreachable!(),
+        file.write_all(format!("Packet #{}\n", i).as_bytes())
+            .unwrap();
+        i += 1;
+        is_first_packet = false;
+
+        for layer in packet {
+            for metadata in layer {
+                if metadata.name() == "tcp.payload" {
+                    // Write the data to the file
+                    file.write_all(
+                        format!(
+                            "{}: {}\n",
+                            metadata.name(),
+                            remove_new_lines(hex_to_string(metadata.value().trim()).as_str())
+                        )
+                        .as_bytes(),
+                    )
+                    .unwrap();
+                } else {
+                    // Write the data to the file
+                    file.write_all(
+                        format!(
+                            "{}: {}\n",
+                            metadata.name(),
+                            remove_new_lines(metadata.value().trim())
+                        )
+                        .as_bytes(),
+                    )
+                    .unwrap();
                 }
-                reader.consume(offset);
             }
-
-            Err(PcapError::Eof) => break,
-
-            Err(PcapError::Incomplete) => {
-                reader.refill().unwrap();
-            }
-
-            Err(err) => panic!("An error while reading the file: {:?}", err),
         }
     }
+}
 
-    println!("\nNumber of blocks: {}", blocks_count);
+/// Convert Hex string to unicode string
+/// For example: 22:73:74:61:74:75:73:22:3a:22:73:74:61:72:74:22 -> "status":"start"
+/// Skip the colon
+fn hex_to_string(hex: &str) -> String {
+    let mut result = String::new();
+    let mut hex = hex.to_string();
+
+    hex.retain(|c| c != ':');
+
+    for i in (0..hex.len()).step_by(2) {
+        let byte = u8::from_str_radix(&hex[i..i + 2], 16).unwrap();
+
+        result.push(byte as char);
+    }
+
+    result
+}
+
+/// Remove new line characters from a string
+fn remove_new_lines(string: &str) -> String {
+    string
+        .replace("\r", "")
+        .replace("\n", "")
+        .replace("\r\n", "")
 }
