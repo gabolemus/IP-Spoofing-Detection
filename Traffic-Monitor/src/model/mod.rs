d2a6df6b728a618979c615387c964fdc9ec0e690
Gabriel Lemus <glemus.stuart@gmail.com>
2022-11-23 16:24:35 -0600

    Update modules structure

--- Traffic-Monitor/src/model/mod.rs
+++ Traffic-Monitor/src/model/mod.rs
@@ -1,13 +1,3 @@
-pub mod frame;
-pub mod http;
-pub mod ip;
 pub mod packet;
-pub mod sll;
-pub mod tcp;
 
-pub use frame::Frame;
-pub use http::HTTP;
-pub use ip::IP;
 pub use packet::Packet;
-pub use sll::SLL;
-pub use tcp::TCP;
