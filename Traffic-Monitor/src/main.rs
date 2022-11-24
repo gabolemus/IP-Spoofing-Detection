3b3030fa3eade1a6c6ef550ba8f275f2e51f991b
Gabriel Lemus <glemus.stuart@gmail.com>
2022-11-24 15:58:55 -0600

    Update todo comment

--- Traffic-Monitor/src/main.rs
+++ Traffic-Monitor/src/main.rs
@@ -6,9 +6,7 @@ fn main() {
     // Enable backtraces for debugging purposes
     env::set_var("RUST_BACKTRACE", "1");
 
-    // Todo: execute the run function infinitely
-    // Todo: once the file has been written once, append the new data to it
-    // instead of overwriting it
+    // Todo: rewrite the csv file in case a new packet has extra fields
 
     // Parse the command line argumets into an `Option<Config>` struct
     if let Some(config) = parse_cmd_args() {
