211067ff45340a2cee64ca69f7079c9f36c57624
Gabriel Lemus <glemus.stuart@gmail.com>
2022-11-23 18:36:01 -0600

    Update modules structure

--- Traffic-Monitor/src/model/mod.rs
+++ Traffic-Monitor/src/model/mod.rs
@@ -3,7 +3,7 @@ pub mod packet;
 pub mod packet_parser;
 pub mod strings_parser;
 
-pub use config::Config;
+pub use config::{parse_cmd_args, Config};
 pub use packet::Packet;
-pub use packet_parser::{parse_cmd_args, run};
+pub use packet_parser::run;
 pub use strings_parser::{hex_to_string, remove_new_lines};
