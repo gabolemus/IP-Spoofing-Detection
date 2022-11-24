8becd2e1835afe644034fd572528b4937a73fa0b
Gabriel Lemus <glemus.stuart@gmail.com>
2022-11-24 10:45:02 -0600

    Change the execution of a program in a loop

--- Traffic-Monitor/src/main.rs
+++ Traffic-Monitor/src/main.rs
@@ -6,16 +6,35 @@ fn main() {
     // Enable backtraces for debugging purposes
     env::set_var("RUST_BACKTRACE", "1");
 
+    // Todo: execute the run function infinitely
+    // Todo: once the file has been written once, append the new data to it
+    // instead of overwriting it
+
     // Parse the command line argumets into an `Option<Config>` struct
     if let Some(config) = parse_cmd_args() {
-        // Run the program with the given configuration
-        exit(match run(&config) {
-            Ok(_) => 0,
-            Err(err) => {
-                error!(config.logger, "Ocurrió un error: {}", err);
-                1
+        let mut loop_count = 1;
+        
+        loop {
+            if loop_count == 5 {
+                break;
             }
-        });
+
+            loop_count += 1;
+            
+            // Run the program with the given configuration
+            match run(&config) {
+                Ok(_) => 0,
+                Err(err) => {
+                    error!(config.logger, "Ocurrió un error: {}", err);
+                    1
+                }
+            };
+
+            // Sleep for 15 seconds
+            std::thread::sleep(std::time::Duration::from_secs(15));
+        }
+
+        exit(0);
     } else {
         // Exit the program with an error code
         exit(1);
