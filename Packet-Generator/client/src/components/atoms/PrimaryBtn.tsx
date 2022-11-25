5a846add4cac48bb754e9579124ebe2ee7100ecd
Gabriel Lemus <glemus.stuart@gmail.com>
2022-11-25 09:50:51 -0600

    Add onClick method

--- Packet-Generator/client/src/components/atoms/PrimaryBtn.tsx
+++ Packet-Generator/client/src/components/atoms/PrimaryBtn.tsx
@@ -5,13 +5,19 @@ import './Button.scss';
 interface PrimaryBtnProps {
   /** The title of the button */
   title: string;
+  /** Onclick function */
+  onClick?: () => void;
   /** Whether the button is disabled or not */
   disabled?: boolean;
 }
 
 const PrimaryBtn = (props: PrimaryBtnProps) => {
   return (
-    <button className="btn btn-primary" disabled={props.disabled || false}>
+    <button
+      className="btn btn-primary"
+      disabled={props.disabled || false}
+      onClick={props.onClick}
+    >
       {props.title}
     </button>
   );
