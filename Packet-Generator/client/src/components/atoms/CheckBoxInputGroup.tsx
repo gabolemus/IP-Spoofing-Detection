11b4706874d7d60930909dae5e92eae16082da9b
Gabriel Lemus <glemus.stuart@gmail.com>
2022-11-25 09:50:44 -0600

    Add ref to checkbox

--- Packet-Generator/client/src/components/atoms/CheckBoxInputGroup.tsx
+++ Packet-Generator/client/src/components/atoms/CheckBoxInputGroup.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 interface CheckBoxInputGroupProps {
   /** The label of the input */
   label: string;
+  /** Reference to the checkbox */
+  checkboxRef: React.RefObject<HTMLInputElement>;
   /** Function to toggle the checkbox */
   toggle: () => void;
 }
@@ -12,7 +14,12 @@ const CheckBoxInputGroup = (props: CheckBoxInputGroupProps) => {
   return (
     <div className="input-group w-15">
       <label htmlFor="infinite-packets">{props.label}</label>
-      <input type="checkbox" id="infinite-packets" onClick={props.toggle} />
+      <input
+        type="checkbox"
+        id="infinite-packets"
+        onClick={props.toggle}
+        ref={props.checkboxRef}
+      />
     </div>
   );
 };
