20f0f74a0241ebeb01f465fd751f3fb4675d9b65
Gabriel Lemus <glemus.stuart@gmail.com>
2022-11-25 10:15:14 -0600

    Add funcitons to send the requests to the server

--- Packet-Generator/client/src/helpers/helpers.ts
+++ Packet-Generator/client/src/helpers/helpers.ts
@@ -1,3 +1,8 @@
+/** This file contains the helper functions used in the project. */
+
+import axios from 'axios';
+import { ExtraPacketData, PacketData, Response } from './interfaces';
+
 /** Style constants */
 const STYLES = {
   maxWidth: '90%',
@@ -20,11 +25,77 @@ const isIPv4 = (str: string): boolean => {
   return regEx.test(str);
 };
 
+/** POST request to send a single spoofed packet */
+const sendSingleSpoofedPacket = async (
+  packet: PacketData
+): Promise<Response> => {
+  // Send the POST request
+  return await sendPostRequest('single-spoofed/', packet);
+};
+
+/** POST request to send multiple spoofed packets */
+const sendMultipleSpoofedPackets = async (
+  packet: ExtraPacketData
+): Promise<Response> => {
+  // Send the POST request
+  return await sendPostRequest('multiple-spoofed/start/', packet);
+};
+
+/** POST request to stop sending multiple spoofed or legitimate packets */
+const stopSendingPackets = async (): Promise<Response> => {
+  // Send the POST request
+  return await sendPostRequest('multiple-spoofed/stop/', {});
+};
+
+/** POST request to send a single legitimate packet */
+const sendSingleLegitimatePacket = async (
+  packet: PacketData
+): Promise<Response> => {
+  // Send the POST request
+  return await sendPostRequest('single-legitimate/', packet);
+};
+
+/** POST request to send multiple legitimate packets */
+const sendMultipleLegitimatePackets = async (
+  packet: ExtraPacketData
+): Promise<Response> => {
+  // Send the POST request
+  return await sendPostRequest('multiple-legitimate/start/', packet);
+};
+
+/** Send a general POST request to the specified endpoint */
+const sendPostRequest = async (
+  endpoint: string,
+  data: PacketData | ExtraPacketData | Record<string, never>
+): Promise<Response> => {
+  // Create the response object from the provided packet data
+  const response = await axios.post(`http://localhost:8080/${endpoint}`, data);
+  // console.log(response.data);
+
+  // Check the response code
+  if (response.status === 200) {
+    return createResponse(true, 'Request sent successfully!');
+  } else {
+    return createResponse(false, 'Error sending request!');
+  }
+};
+
+/** Create a response object */
+const createResponse = (success: boolean, message: string): Response => ({
+  success,
+  message,
+});
+
 /** General helpers */
 const helpers = {
   STYLES,
   PALETTE,
   isIPv4,
+  sendSingleSpoofedPacket,
+  sendMultipleSpoofedPackets,
+  stopSendingPackets,
+  sendSingleLegitimatePacket,
+  sendMultipleLegitimatePackets,
 };
 
 export default helpers;
